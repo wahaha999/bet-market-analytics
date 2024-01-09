413593e7a3c798c60ef4875706fcea431749580b
Xiang Fang <xiang.fang@mercuriusinc.com>
2024-01-09 13:44:00 -0500

    update bet-market

--- src/constant.tsx
+++ src/constant.tsx
@@ -1,1 +1,1 @@
-export const API_URL = "http://144.172.74.54:5000"
+export const API_URL = "http://localhost:5000"
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import theme from './styles/theme.ts'
-import { ThemeProvider } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <ThemeProvider theme={theme}>
